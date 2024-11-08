7d7797415cd4129639834a375ceece0882032e32
Gustavo Amarante <32441206+gusamarante@users.noreply.github.com>
2024-11-08 08:07:21 -0300

    Update example.py

--- example.py
+++ example.py
@@ -1,6 +1,6 @@
 from bayesfm import *
 
-# ===== READ DATA =====
+# ===== Read Data =====
 # Excess return of Fama-French portfolios
 ports = get_ff25p()
 rf = get_ffrf()
@@ -8,11 +8,11 @@ ports = ports.sub(rf, axis=0)
 ports = ports.dropna()
 ports.columns = [f"FF{(s - 1) * 5 + v}" for s, v in ports.columns]
 
-# Fama-Frenc 5 factors
+# Fama-French 5 factors
 facts = get_ff5f()
 
 
-# ===== BAYESIAN Fama-MacBeth =====
+# ===== Bayesian Fama-MacBeth =====
 model = BFM(
     assets=ports,
     factors=facts,
